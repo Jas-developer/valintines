f1215f240ee8205973274ddc0268d9c2800a1d1e
Jason <devop.jas@gmail.com>
2024-02-12 00:28:43 +0800

    cringe

---
+++ for_criza/src/App.tsx
@@ -0,0 +1,100 @@
+import { motion } from "framer-motion";
+import background from "./assets/image/background.jpg";
+import {
+  Cringe,
+  Sentence_A,
+  Sentence_B,
+  Sentence_C,
+  Sentence_D,
+  Sentence_E,
+  Sentence_F,
+  Sentence_G,
+  Sentence_H,
+  Sentence_I,
+  Song_Suggest,
+} from "./components/Sections";
+
+export default function App() {
+  return (
+    <section
+      className=" w-[100vw] h-[100vh] font-serif"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      {/* first  sentence */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1 }}
+        variants={{
+          hidden: { opacity: 0, x: -100 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Sentence_A />
+      </motion.div>
+      {/* Second sentence */}
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_B />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_C />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_D />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_E />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_F />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_G />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_H />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Sentence_I />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Song_Suggest />
+      </div>
+      <div
+        style={{ backgroundImage: `url(${background})` }}
+        className="w-full h-full bg-pink-500  flex justify-center items-center"
+      >
+        <Cringe />
+      </div>
+    </section>
+  );
+}
---
+++ for_criza/src/components/Sections.tsx
@@ -0,0 +1,214 @@
+import { motion } from "framer-motion";
+
+export const Sentence_A = () => {
+  return (
+    <div className="bg-pink-400 w-[90%] rounded-tl-[50%] rounded-br-[50%] h-[100px] flex justify-center items-center">
+      <span className=" text-md md:text-3xl text-white">
+        Hi,cringe confession incoming HAHA
+      </span>
+    </div>
+  );
+};
+//Sentence_B
+export const Sentence_B = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        I know you've caught me looking at your desk many times.
+      </span>
+    </motion.div>
+  );
+};
+
+// Sentence_C
+export const Sentence_C = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        It's your pretty eyes' fault😾
+      </span>
+    </motion.div>
+  );
+};
+
+// Sentence_D
+export const Sentence_D = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">just kidding</span>
+    </motion.div>
+  );
+};
+
+// Sentence_E
+export const Sentence_E = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        One of the reasons why I may admire you is I can see that you have your
+        priorities straight
+      </span>
+    </motion.div>
+  );
+};
+
+// Sentence_F
+export const Sentence_F = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        Keep doing that thing, madam.
+      </span>
+    </motion.div>
+  );
+};
+export const Sentence_G = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        I did not make this to pursue a relationship with you;
+      </span>
+    </motion.div>
+  );
+};
+
+// Sentence_H
+export const Sentence_H = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        I just find you attractive in many ways
+      </span>
+    </motion.div>
+  );
+};
+
+// Sentence_I
+export const Sentence_I = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 w-[80%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span className="text-md md:text-3xl text-white">
+        and that's it for now
+      </span>
+    </motion.div>
+  );
+};
+
+export const Song_Suggest = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 text-md md:text-2xl text-white w-[90%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span>
+        "Oh ang isang ka tulad mo, Ay di na dapat pang pakawalan. Alam mo bang
+        pag naging tayo,araw-araw kitang liligawan🎶" <br /> -TJ Monterde
+      </span>
+    </motion.div>
+  );
+};
+
+export const Cringe = () => {
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.5 }}
+      transition={{ duration: 1 }}
+      variants={{
+        hidden: { opacity: 0, x: -100 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      className="bg-pink-400 text-md md:text-2xl text-white w-[90%] rounded-tl-[50%] rounded-br-[50%] h-[40%] p-8 flex justify-center items-center"
+    >
+      <span>Cringe😭AHAHA</span>
+    </motion.div>
+  );
+};
